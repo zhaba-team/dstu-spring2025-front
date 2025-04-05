95805d6698a98a7d8d2ee60403a91c5f236033b1
Александр Коваленко <alexanderkovalenko@MacBook-Air-Aleksandr-2.local>
2025-04-05 12:41:47 +0300

    style fixes

--- src/components/PairTable/PairTable.vue
+++ src/components/PairTable/PairTable.vue
@@ -6,6 +6,11 @@ const products = [
   {
     id: '1000',
     color: 'red1',
+    chanceWithPurple: "75%",
+    chanceWithOrange: "10%",
+    chanceWithGreen: "75%",
+    chanceWithRed: "---",
+    chanceWithBlue: "25%",
   },
   {
     id: '1000',
@@ -21,17 +26,22 @@ const products = [
 </script>
 
 <template>
-  <DataTable :value="products" tableStyle="min-width: 50rem">
+  <h1>Таблица
+    вероятности
+    занятия 1го и 2го
+    мест</h1>
+  <DataTable :value="products" tableStyle="min-width: 60rem; border-radius: 20px">
     <Column field="color" header=""></Column>
-    <Column field="red1" header="red1"></Column>
-    <Column field="blue" header="blue"></Column>
-    <Column field="orange" header="orange"></Column>
-    <Column field="green" header="green"></Column>
-    <Column field="purple" header="purple"></Column>
+    <Column field="chanceWithRed" header="red"></Column>
+    <Column field="chanceWithBlue" header="blue"></Column>
+    <Column field="chanceWithOrange" header="orange"></Column>
+    <Column field="chanceWithGreen" header="green"></Column>
+    <Column field="chanceWithPurple" header="purple"></Column>
     <Column field="black" header="black"></Column>
   </DataTable>
 </template>
 
 <style scoped>
+
 
 </style>
